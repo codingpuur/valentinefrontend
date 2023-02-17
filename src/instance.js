f9ead98faa88dedd97df6335beef3c486fd7b84a
shubham chaudhari <70107428+codingpuur@users.noreply.github.com>
2023-02-17 13:00:06 +0530

    Update instance.js

--- src/instance.js
+++ src/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance =axios.create({
-    baseURL:"https://valentinesaga.onrender.com"
+    baseURL:"https://backend-fiuc.onrender.com"
 })
 
 export default instance;
