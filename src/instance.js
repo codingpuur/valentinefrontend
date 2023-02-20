f0471c36fab32d661f5cbb2c2f69524ae0a41bda
shubham chaudhari <70107428+codingpuur@users.noreply.github.com>
2023-02-20 08:20:25 +0530

    Update instance.js

--- src/instance.js
+++ src/instance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance =axios.create({
     baseURL:"https://backend-fiuc.onrender.com",
-    headers: {"Authorization" : "Bearer" + localStorage.getItem("token")}
+    headers: {"Authorization" : "Bearer" + localStorage.getItem("usertoken")}
 })
 
 export default instance;
