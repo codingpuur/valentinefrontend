f686a66bf90470fde4cdd5dff1c3320c442db60c
shubham chaudhari <70107428+codingpuur@users.noreply.github.com>
2023-02-20 06:27:54 +0530

    Update userAction.js

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -109,7 +109,7 @@ export const loadUser = () => async (dispatch) => {
         dispatch({ type: LOAD_USER_REQUEST });
           const config = {
       headers: {
-        Authorization: `Bearer ${userInfo.name}`,
+        Authorization: `Bearer `+data.token,
       },
     };
 
